4809a547d882674c2c4c06cf97ed876962413768
Gustavo Takahashi <gustavo.takahashi94@gmail.com>
2024-07-04 13:14:07 -0300

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button, TouchableOpacity } from "react-native";
+import { Feather } from "react-native-vector-icons";
+import LottieView from "lottie-react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    let animacao = React.createRef();
+
+    function comecarAnimacao() {
+        animacao.current.play();
+    }
+
+    return (
+        <View style={styles.container}>
+            <TouchableOpacity onPress={comecarAnimacao}>
+                <Feather name="play" size={60} color="black" />
+            </TouchableOpacity>
+            <LottieView
+                source={require("./assets/check.json")}
+                style={{ width: 200, height: 200 }}
+                loop={false}
+                ref={animacao}
+            />
+            <Button title="Comecar animacao" onPress={comecarAnimacao} />
+
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
 });
